d1b38ba496a8ef0705c9e18225309d565b79edaa
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-02-27 17:05:38 +0900

    5.2クラスの最終コード

--- src/chapter05/Character.ts
+++ src/chapter05/Character.ts
@@ -3,5 +3,37 @@
  * ゲームキャラクターを表現する
  */
 export class Character {
-  
-}
+    // 名前を表すプロパティ(読取り専用)
+    readonly name: string = "名無し";
+    // 体力を表すプロパティ
+    hp: number = 100;
+    // 技の一覧を表すプロパティ
+    skills: string[] = [];
+
+    /**
+     * コンストラクタ
+     * @param name 名前
+     * @param hp 体力
+     */
+    constructor(name: string, hp: number) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    /**
+     * 自己紹介をするメソッド
+     */
+    introduce(): void {
+        console.log(`私は ${this.name}。体力は残り ${this.hp} です。`);
+    }
+
+    /**
+     * ダメージを受けるメソッド
+     * @param damage ダメージ
+     */
+    takeDamage(damage: number): void {
+        this.hp -= damage; // 自分のHPを減らす
+        if (this.hp < 0) this.hp = 0;
+        console.log(`${this.name} は ${damage} のダメージを受けた！`);
+    }
+}   
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { Character } from "./chapter05/Character.js";
 
-// インスタンスの生成
-const hero = new Character(); 
+/**
+ * インスタンスの生成例
+ */
+// 1. 勇者という「実体」を作る
+const hero = new Character("勇者", 100);
+
+// 2. 魔王という「別の実体」を作る
+const boss = new Character("魔王", 500);
+
+// それぞれ独立しているので、片方のHPが減っても、もう片方には影響しない
+hero.takeDamage(20);
+console.log(hero.hp); // 80
+console.log(boss.hp); // 500 (魔王は無傷！)
