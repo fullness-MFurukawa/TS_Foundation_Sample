2026c2dc302663976471dfc7ec8f01020b5102ae
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-02-28 11:47:12 +0900

    アクセス修飾子の付加

--- src/chapter05/Character.ts
+++ src/chapter05/Character.ts
@@ -1,14 +1,51 @@
 /**
- * 5-2.クラス宣言
- * ゲームキャラクターを表現する
+ * 5-3. アクセス修飾子
+ * カプセル化を適用したゲームキャラクタークラス
  */
 export class Character {
+    
+    /**
+     * 内部保持用の変数名は慣習に従い「_」を付ける
+     */
     // 名前を表すプロパティ(読取り専用)
-    readonly name: string = "名無し";
+    private _name: string  = "名無し";
     // 体力を表すプロパティ
-    hp: number = 100;
+    private _hp: number = 100;
     // 技の一覧を表すプロパティ
-    skills: string[] = [];
+    private _skills: string[] = [];
+
+    /**
+     * _nameのゲッター
+     */
+    get name(): string {
+        return this._name;
+    }
+
+    /**
+     * _hpのゲッター
+     */
+    get hp(): number {
+        return this._hp;
+    }
+
+    /**
+     * _hpのセッター
+     */
+    set hp(value: number) {
+        // セッターの中で値のチェックを行う
+        if (value < 0) {
+            this._hp = 0;
+        } else {
+            this._hp = value;
+        }
+    }
+
+    /**
+     * _skillsのゲッター
+     */
+    get skills(): string[] {
+        return this._skills;
+    }
 
     /**
      * コンストラクタ
@@ -16,15 +53,15 @@ export class Character {
      * @param hp 体力
      */
     constructor(name: string, hp: number) {
-        this.name = name;
-        this.hp = hp;
+        this._name = name;
+        this._hp = hp;
     }
 
     /**
      * 自己紹介をするメソッド
      */
     introduce(): void {
-        console.log(`私は ${this.name}。体力は残り ${this.hp} です。`);
+        console.log(`私は ${this._name}。体力は残り ${this._hp} です。`);
     }
 
     /**
@@ -32,8 +69,8 @@ export class Character {
      * @param damage ダメージ
      */
     takeDamage(damage: number): void {
-        this.hp -= damage; // 自分のHPを減らす
-        if (this.hp < 0) this.hp = 0;
-        console.log(`${this.name} は ${damage} のダメージを受けた！`);
+        this._hp -= damage; // 自分のHPを減らす
+        if (this._hp < 0) this._hp = 0;
+        console.log(`${this._name} は ${damage} のダメージを受けた！`);
     }
 }   
